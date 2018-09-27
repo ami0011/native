070957113b339588070efd257edabfb3521b245b
Amitesh Chauhan <ami070ipec@gmail.com>
2018-09-27 14:38:40 +0800

    initial

---
+++ native-base-theme/components/Content.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import variable from '../variables/platform';
+
+export default (variables = variable) => {
+  const contentTheme = {
+    flex: 1,
+    backgroundColor: 'transparent',
+    'NativeBase.Segment': {
+      borderWidth: 0,
+      backgroundColor: 'transparent',
+    },
+  };
+
+  return contentTheme;
+};
